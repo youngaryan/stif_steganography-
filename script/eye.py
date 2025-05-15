115593409e63200925e5e1da71304aae32c10e1d
Aryan Golbaghi <aryangolbaghi@gmail.com>
2025-05-15 12:12:32 +0100

    basic interface is impl

--- script/eye.py
+++ script/eye.py
@@ -1,38 +1,42 @@
 from embeder import WatermarkEmbedder
 from temper_detector import TemperDetector
+from interface import InterFace
 
 
 if __name__ =="__main__":
 
-    embedder = WatermarkEmbedder(watermark_image_path="images/watermark_A_5x5.png", segment_size=5, carrier_image_path="images/che.png")
+    # embedder = WatermarkEmbedder(watermark_image_path="images/watermark_A_5x5.png", segment_size=5, carrier_image_path="images/che.png")
     
-    # embedder.show_image(0) #Carrier image
-    # embedder.show_image(1) #Watermark image
-    # embedder.show_image(2) #watermarked image(modified carrier image)
-    # embedder.show_image(3) # Carrier image with keypoints
-    # embedder.show_image(4)
-    # embedder.show_image(5) #Rotated Carrier Image
-    # embedder.show_image(6) #watermarked image(modified rotated carrier image)
+    # # embedder.show_image(0) #Carrier image
+    # # embedder.show_image(1) #Watermark image
+    # # embedder.show_image(2) #watermarked image(modified carrier image)
+    # # embedder.show_image(3) # Carrier image with keypoints
+    # # embedder.show_image(4)
+    # # embedder.show_image(5) #Rotated Carrier Image
+    # # embedder.show_image(6) #watermarked image(modified rotated carrier image)
 
 
-    # embedder.show_image(7) #scaled imge
-    # embedder.show_image(8)
+    # # embedder.show_image(7) #scaled imge
+    # # embedder.show_image(8)
 
-    # embedder.show_image(9) #cropped imge
-    # embedder.show_image(10)
+    # # embedder.show_image(9) #cropped imge
+    # # embedder.show_image(10)
 
-    # print("varyfy rotation ",embedder.varify_watermark(img=embedder.modified_carrier_image_rotated))
-    # print("varyfy scaled ",embedder.varify_watermark(img=embedder.modified_carrier_image_scaled))
-    # print("varyfy cropped ",embedder.varify_watermark(img=embedder.modified_carrier_image_cropped))
+    # # print("varyfy rotation ",embedder.varify_watermark(img=embedder.modified_carrier_image_rotated))
+    # # print("varyfy scaled ",embedder.varify_watermark(img=embedder.modified_carrier_image_scaled))
+    # # print("varyfy cropped ",embedder.varify_watermark(img=embedder.modified_carrier_image_cropped))
     
     
-    # print("varyfy actual embded ",embedder.varify_watermark(img_path=embedder.modified_carrier_image_path))
+    # # print("varyfy actual embded ",embedder.varify_watermark(img_path=embedder.modified_carrier_image_path))
 
-    detector =TemperDetector(embedder,threshold=0.05)
+    # detector =TemperDetector(embedder,threshold=0.05)
 
-    #true return
-    print(detector.detect_temper(modified_img_path="images/che.png")['tampered'])
+    # #true return
+    # print(detector.detect_temper(modified_img_path="images/che.png")['tampered'])
 
 
-    #false return
-    print(detector.detect_temper(modified_img_path="res/embeded_watermatks.png")['tampered'])
+    # #false return
+    # print(detector.detect_temper(modified_img_path="res/embeded_watermatks.png")['tampered'])
+
+    ic = InterFace()
+    ic.mainloop()
---
+++ script/interface.py
@@ -0,0 +1,42 @@
+import tkinter as tk
+from tkinter import filedialog
+from embeder import WatermarkEmbedder
+from temper_detector import TemperDetector
+
+
+class InterFace(tk.Tk):
+    def __init__(self, screenName = None, baseName = None, className = "Tk", useTk = True, sync = False, use = None):
+        super().__init__(screenName, baseName, className, useTk, sync, use)
+        self.title("watermark embeder (COM3001)")
+        tk.Button(self,text="Embed", command=self.embed).grid(row=0,column=0)
+        tk.Button(self,text="verify", command=self.verify).grid(row=0,column=1)
+        tk.Button(self,text="detect temper", command=self.temper_detect).grid(row=0,column=2)
+        self.embeder:WatermarkEmbedder=None
+
+    
+    def _load_paths(self):
+        carrier =filedialog.askopenfilename(title="choose carrier image")
+        wm= filedialog.askopenfilename(title="choose watermark image ")
+        return carrier, wm
+    
+    def embed(self):
+        carrier, wm = self._load_paths()
+        #TODO need to change embeder class maybe to only apply the watermark when the function is called
+        self.embeder = WatermarkEmbedder(carrier_image_path=carrier, watermark_image_path=wm, segment_size=5)
+        tk.messagebox.showinfo("Done", f"Saved at {self.embeder.modified_carrier_image_path}")
+
+    def verify(self):
+        path_to_var = filedialog.askopenfilename()
+        
+        check = self.embeder.varify_watermark(img_path=path_to_var)
+        print("varifed" if check else "not varifed!")
+
+
+    def temper_detect(self):
+        path_to_var = filedialog.askopenfilename()
+
+        td = TemperDetector(embeder=self.embeder)
+        check = td.detect_temper(path_to_var)
+        print("temperd!" if check["tampered"] else "not tampered")
+
+
